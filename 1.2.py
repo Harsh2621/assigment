80c28b68eb0e5e4af64f21ceda3525035d69de16
Harsh2621 <shrimaliharshang1@gmail.com>
2023-11-08 11:15:20 +0530

    code file

---
+++ 1.2.py
@@ -0,0 +1,8 @@
+# Accept the temperature in Fahrenheit from the user
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+# Convert Fahrenheit to Celsius
+celsius = (fahrenheit - 32) * 5/9
+
+# Print the result
+print("Temperature in Celsius:", celsius)
